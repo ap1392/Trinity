b13ef670a764feb423f35397e42ffb70bd523d93
Aditya Pathak <adityap029@gmail.com>
2023-10-28 18:07:43 -0400

    Added an upload image that gets an image from your computer

--- trinity-wallpaper/src/app/page.tsx
+++ trinity-wallpaper/src/app/page.tsx
@@ -1,7 +1,46 @@
+"use client"; // This is a client component 👈🏽
+
+import { useState } from "react";
 import Image from "next/image";
 
-export default function Home() {
+const Home = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setSelectedImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      {selectedImage && (
+        <div>
+          <h2>Resized Image:</h2>
+          <Image
+            src={selectedImage}
+            alt="Resized Image"
+            width={300} // Specify the desired width
+            height={200} // Specify the desired height
+          />
+        </div>
+      )}
+    </main>
   );
-}
+};
+
+const WallpaperResizer = () => {
+  return (
+    <div>
+      <Home resizedImage={undefined} />
+    </div>
+  );
+};
+
+export default WallpaperResizer;
